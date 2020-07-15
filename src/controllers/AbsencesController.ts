50506204ce3ef8a7bdaa6939f9b376cdb5136b16
Bruno <mguimaraesbruno@gmail.com>
2020-07-15 16:14:46 -0300

    adds abscence props

--- src/controllers/AbsencesController.ts
+++ src/controllers/AbsencesController.ts
@@ -7,9 +7,20 @@ class AbsencesController {
     async index(request: Request, response: Response) {
         const absences = await knex('absences').select('*');
 
-        const serializedAbsences= absences.map(absence => {
+        const serializedAbsences = absences.map(absence => {
             return {
                 id: absence.id,
+                admitterId: absence.admitterId,
+                admitterNote: absence.admitterNote,
+                confirmedAt: absence.confirmedAt,
+                createdAt: absence.createdAt,
+                crewId: absence.crewId,
+                endDate: absence.endDate,
+                memberNote: absence.memberNote,
+                rejectedAt: absence.rejectedAt,
+                startDate: absence.startDate,
+                type: absence.type,
+                userId: absence.userId
             };
         });
 
@@ -18,3 +29,4 @@ class AbsencesController {
 }
 
 export default AbsencesController;
+

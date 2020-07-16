5a3528b49de94a5e8c3f84b1b8df233129510cc8
Bruno <mguimaraesbruno@gmail.com>
2020-07-16 12:17:43 -0300

    manually write .ics file

--- src/controllers/CalendarController.ts
+++ src/controllers/CalendarController.ts
@@ -12,8 +12,6 @@ class CalendarController {
     const members = await knex('members').select('*');
     const absences = await knex('absences').select('*');
 
-
-    const cal = ical({ domain: 'localhost:3333', name: 'my first iCal' });
     const events = absences.map(absence => ({
       start: absence.startDate,
       end: absence.endDate,
@@ -25,14 +23,30 @@ class CalendarController {
           : "sick"}`,
       name: members.find(member => member.userId === absence.userId)!.name,
     }));
-    cal.events(events)
-    const ics = cal._generate();
-    fs.writeFile('calendar.ics', ics, function (err: any) {
-      if (err) throw err;
-      console.log('File is created successfully.');
-    }); 
-    const file = `./calendar.ics`;
-    response.download(file);
+    try {
+      const icsFile = fs.createWriteStream('./temp/calendar.ics')
+      icsFile.write('BEGIN:VCALENDAR\n');
+      icsFile.write('VERSION:1.0\n');
+      events.forEach((event) => {
+        icsFile.write('BEGIN:VEVENT\n');
+        icsFile.write(`SUMMARY:${event.title}\n`);
+        icsFile.write(`DESCRIPTION:${event.name}\n`);
+        icsFile.write(`UID:${event.userId}\n`);
+        icsFile.write('TZID:W. Europe Standard Time \n');
+        icsFile.write(`DTSTART:${event.start.replace(/-/g, '')}\n`);
+        icsFile.write(`DTEND:${event.end.replace(/-/g, '')}\n`);
+        icsFile.write('END:VEVENT\n');
+      });
+      icsFile.write('END:VCALENDAR\n');
+      icsFile.end();
+
+      icsFile.on('close', () => {
+        return response.status(200).download('./temp/calendar.ics');
+      });
+    } catch (err) {
+      return response.status(500).json({ error: 'Couldn`t get calendar ics'  });
+    }
+
   }
 }
 
